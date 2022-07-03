a9e464984e37da95cdcc70bbe6d9c4e18f5b31e2
Rajan Theengh <theenghrajan@gmail.com>
2022-07-03 14:56:07 +0545

    About hero responsive updated

--- www/wp-content/themes/reiki-lafayette/footer.php
+++ www/wp-content/themes/reiki-lafayette/footer.php
@@ -11,20 +11,8 @@
 
 ?>
 	</main>
-	<footer id="colophon" class="site-footer mt-auto">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'reiki-lafayette' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'reiki-lafayette' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'reiki-lafayette' ), 'reiki-lafayette', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer class="site-footer mt-auto">
+		
 	</footer><!-- #colophon -->
 </div><!-- /.site-wrapper -->
 
--- www/wp-content/themes/reiki-lafayette/template-pages/tpl-about-inge.php
+++ www/wp-content/themes/reiki-lafayette/template-pages/tpl-about-inge.php
@@ -1,25 +1,25 @@
 <?php
-/* Template Name: About */
+/* Template Name: About Inge */
 
 get_header();
 ?>
 
-<section class="hero container-alt">
+<section class="hero hero-mobile-overlap container-alt overlay-mobile overlay-mobile--bottom">
     <div class="row align-items-center">
-        <picture class="col-md-6 hero__img-block img-fluid">
+        <picture class="col-md-6 img-block img-fluid">
             <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/about-hero-feature.webp" type="image/webp">
             <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/about-hero-feature.jpg" type="image/jpg">
             <source srcset="<?php echo site_url(); ?>/media/about-hero-feature-mobile.webp" type="image/webp">
             <source srcset="<?php echo site_url(); ?>/media/about-hero-feature-mobile.jpg" type="image/jpg">
             <img src="<?php echo site_url(); ?>/media/about-hero-feature.jpg" alt="Home Hero Background" width="718" height="865" loading="lazy">
         </picture>
-        <div class="col-md-6 hero__content-block text-center">
+        <div class="col-md-6 content-block text-center">
             <div class="font-nelphim font-size-lg block-width-350 mx-auto">
                 <h1 class="heading-primary mb-1 mb-lg-2 mb-xl-4">
                     <span>Hi</span>
                 </h1> <!-- /.heading-primary -->
                 <div class="border-mini border-mini--center mb-1 mb-lg-2">
-                    <p>I’m Inge. I’m the creator, founder & mentor behind Reiki Montreal.</p>
+                    <p>I’m Inge. <br>I’m the creator, founder & mentor behind Reiki Montreal.</p>
                 </div>
             </div>
         </div>
