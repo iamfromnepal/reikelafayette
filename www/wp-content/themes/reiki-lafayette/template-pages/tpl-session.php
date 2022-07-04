39a8c4472a897aefbdb001c07dd5b47922e962d7
Rajan Theengh <theenghrajan@gmail.com>
2022-07-05 02:31:51 +0545

    Session dektop version updated

--- www/wp-content/themes/reiki-lafayette/template-pages/tpl-session.php
+++ www/wp-content/themes/reiki-lafayette/template-pages/tpl-session.php
@@ -63,7 +63,14 @@ get_header();
     </div>
 </section> <!-- /.session-about -->
 
-<section class="session-info">
+<section class="session-info container-alt has-bg-img pt-2 pb-3">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-info-bg.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-info-bg.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/session-info-bg-mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/session-info-bg-mobile.jpg" type="image/jpg">
+        <img src="<?php echo site_url(); ?>/media/session-info-bg.jpg" alt="Home Hero Background" width="1440" height="669" loading="lazy">
+    </picture>
     <div class="container">
         <div class="content-block mx-auto counter-list text-center">
             <h2 class="heading-secondary">
@@ -72,7 +79,7 @@ get_header();
             </h2>
             <p>At Reiki Montreal, we all:</p>
 
-            <ul class="row justify-content-between">
+            <ul class="row justify-content-between mb-4">
                 <li class="col-md-3">Practice Reiki</li>
                 <li class="col-md-5">Use energy healing in our sessions with you (sometimes in combination with our other modalities such as osteopathy, massage, intuitive readings, coaching & more).</li>
                 <li class="col-md-3">Have the experience of transforming stuck energy into personal growth.</li>
@@ -80,11 +87,102 @@ get_header();
                 <li class="col-md-6">Receive mentoring in view of giving you the best of ourselves.</li>
             </ul>
 
-            <h3>As for the rest?</h3>
-            <p>We’re all different people, with different sensitivities, styles & specialties.</p>
+            <div class="foot">
+                <h3>As for the rest?</h3>
+                <p>We’re all different people, with different sensitivities, styles & specialties.</p>
+            </div>
         </div>
     </div>
 </section> <!-- /.session-info -->
+
+<div class="session-mini-block container pt-6 pb-10">
+    <div class="mini-block">
+        <p>Find your healing ally by answering the questions below or by browsing our team.</p>
+    </div>
+</div> <!-- /.session-mini-block -->
+
+<section class="session-team has-bg-img container-alt pt-3 pb-5">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-team-bg.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-team-bg.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/session-team-bg-mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/session-team-bg-mobile.jpg" type="image/jpg">
+        <img src="<?php echo site_url(); ?>/media/session-team-bg.jpg" alt="Home Hero Background" width="1440" height="669" loading="lazy">
+    </picture>
+    <div class="content-block text-white text-center">
+        <h2>Our <span>Team</span></h2>
+        <p>Finding the right practitioner for you is almost everything. Working with someone who really gets you is a gift you give yourself potentially for years to come. Trust your intuition while browsing the practitioners on our team.</p>
+        <p>… but right now you just want to try Reiki and you don’t REALLY care with whom? As long as it’s in the right price range, neighborhood? We’ve got you covered there too.</p>
+
+        <a href="#" class="btn btn-primary">Click Here</a>
+    </div>
+</section> <!-- /.session-team -->
+
+<section class="session-bleeding container-alt bg-primary pt-4 pb-3">
+    <div class="container">
+        <div class="box-quote box-quote--pri mx-auto text-center">
+            <p>We do our best to show up as our best selves so you can too</p>
+        </div>
+    </div>
+</section> <!-- /section-bleeding -->
+
+<section class="session-founder pt-3">
+    <div class="container">
+        <h2 class="heading-secondary text-center mb-2">
+            All our practitioners
+            <span>Receive Mentoring</span>
+        </h2>
+    </div>
+    <div class="container-alt has-bg-img">
+        <picture class="bg-img">
+            <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-founder-bg.webp" type="image/webp">
+            <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-founder-bg.jpg" type="image/jpg">
+            <source srcset="<?php echo site_url(); ?>/media/session-founder-bg-mobile.webp" type="image/webp">
+            <source srcset="<?php echo site_url(); ?>/media/session-founder-bg-mobile.jpg" type="image/jpg">
+            <img src="<?php echo site_url(); ?>/media/session-founder-bg.jpg" alt="Home Hero Background" width="1438" height="435" loading="lazy">
+        </picture>
+        <div class="inner text-white text-center mx-auto">
+            <h3>
+                <span>About</span>
+                Inge Broer
+            </h3>
+            <p>Founder of Reiki Montreal & mentor of the other healing practitioners.</p>
+        </div>
+    </div>
+</section> <!-- /.section-founder -->
+
+<section class="session-founderInfo container pt-5 pb-4">
+    <div class="content-block block-width-625 mx-auto text-center">
+        <p>Since founding Reiki Montreal in 2013, Inge has worked with thousands of clients and trained hundreds of Reiki practitioners.</p>
+
+        <div class="box-quote box-quote--md mx-auto text-center mb-1">
+            <p>She’s seen miracles unfold and receives deep satisfaction in knowing how many lives she’s been able to touch.</p>
+        </div>
+
+        <p>Now, she’s devoted to put her experience, skills & knowledge to the best use by mentoring her practitioners to be the best healers they can be. When you work with a healer at Reiki Montreal, you also receive the benefit form knowing they are guided by an experienced healer.</p>
+
+        <p>All Reiki Montreal practitioners have done at least some of their training with Inge & receive mentoring from her.</p>
+    </div>
+</section> <!-- /.section-founderInfo -->
+
+<section class="session-healing container-alt has-bg-img py-2">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-healing-bg-.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/session-healing-bg-.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/session-healing-bg--mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/session-healing-bg--mobile.jpg" type="image/jpg">
+        <img src="<?php echo site_url(); ?>/media/session-healing-bg-.jpg" alt="Home Hero Background" width="1438" height="712" loading="lazy">
+    </picture>
+    <div class="inner text-center mx-auto">
+        <h2>What is <br>Energy Healing</h2>
+    </div>
+</section> <!-- /.session-healing -->
+
+<div class="container py-2">
+    <div class="block-width-625 text-center mx-auto">
+        <p>Energy healing operates on the notion that everything is energy (or vibrating information) and works in the present moment to help people reconnect to the truth of who they are (at our core: we are love). Different energy healing modalities include Reiki, Visualization (such as inner child healing), Tapping/EFT, Hypnosis and more. Different people respond to different modalities at different times so we make it our business to have a full toolkit at your disposal.</p>
+    </div>
+</div>
 
 <?php
 get_footer();
