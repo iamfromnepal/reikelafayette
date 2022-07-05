ce071cef25659f30d0521a08370447b3b85ddae4
Sumit Gurung <ss.dev.n118@gmail.com>
2022-07-05 23:05:37 +0545

    Chakra Guide page updated

--- www/wp-content/themes/reiki-lafayette/template-pages/tpl-chakra-guide.php
+++ www/wp-content/themes/reiki-lafayette/template-pages/tpl-chakra-guide.php
@@ -82,11 +82,149 @@ get_header();
 
             <h2 class="heading-secondary text-center">
                 <span>The Root Chakra </span>
-                – Security, Money, Food, Shelter. 
+                – Security, Money, Food, Shelter.
             </h2>
         </div>
     </div>
 </section>
+
+<section class="container info-col py-xl-5 py-lg-4 py-sm-3 py-2">
+    <div class="row align-items-center">
+        <div class="col-lg-5">
+            <div class="info-col__box list-unstyled text-center mb-lg-0 mb-sm-3 mb-2">
+                <h4>The Basics for the Root Chakra</h4>
+
+                <ul>
+                    <li><span>Location:</span> The root chakra flows out of the perineum, through the legs and feet into the earth.</li>
+                    <li><span>Colour:</span> Red or Brown-Red.</li>
+                    <li><span>Basic Right:</span> Right to be here & Right to have.</li>
+                    <li><span>Body parts associated:</span> Bones, skeletal structure, rectum, coccyx, perineum, legs, feet, urethra, urinary bladder, genitals, adrenal glands, adrenal cortex.</li>
+                </ul>
+            </div> <!-- /.info-col__box -->
+        </div>
+        <div class="col-lg-7">
+            <div class="info-col__content pl-xl-3 pl-lg-2">
+                <p>The Root Chakra is our ground, energetically speaking. It’s our foundation, our ability to stand on our two feet. It governs our ability to survive and to feel safe in the physical world. Because the root chakra is developed in early infancy, it is formed first. It contains all the information in those early months where the challenge is to be here, as a physical being.
+                </p>
+                <p>
+                    As a physical being, we experience hunger, discomfort and pain. The questions we answer are: ” Is this a safe place?” “Am I welcome here?” Everything in our surroundings pertaining to these questions gets stored in the root chakra.
+                </p>
+                <p>
+                    As we grow older, this sense of security manifests in the way we take care of ourselves. It plays out in our relationships with food, with shelter, with money – the things we need to survive.
+                </p>
+            </div> <!-- /.info-col__content -->
+        </div>
+    </div>
+</section> <!-- /.info-col -->
+
+<section class="cta-banner has-bg-img py-lg-4 py-lg-3 py-sm-2 py-1">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/cta-banner-bg.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/cta-banner-bg.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/cta-banner-bg-xs.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/cta-banner-bg-xs.jpg" type="image/jpg">
+        <img width="1440" height="302" src="<?php echo site_url(); ?>/media/cta-banner-bg.jpg" loading="lazy" alt="Loving Hand">
+    </picture> <!-- /.bg-img -->
+    <div class="container">
+        <div class="cta-banner__content max-width-330 text-center mx-sm-0 mx-auto">
+            <h2 class="heading-secondary text-center">
+                <span>Are you</span>
+                loving this content?
+            </h2>
+            <a href="#" class="btn btn-primary">Sign Up Here</a>
+            <p>to get instant access to the recording of a (free) webinar about the 7 chakras where you’re going to get so much more. </p>
+        </div>
+    </div>
+</section>
+
+<section class="container info-col py-xl-5 py-lg-4 py-sm-3 py-2">
+    <div class="row align-items-center">
+        <div class="col-lg-5">
+            <div class="info-col__box list-unstyled text-center">
+                <h4>The Basics for the Root Chakra</h4>
+
+                <ul>
+                    <li><span>Location:</span> The root chakra flows out of the perineum, through the legs and feet into the earth.</li>
+                    <li><span>Colour:</span> Red or Brown-Red.</li>
+                    <li><span>Basic Right:</span> Right to be here & Right to have.</li>
+                    <li><span>Body parts associated:</span> Bones, skeletal structure, rectum, coccyx, perineum, legs, feet, urethra, urinary bladder, genitals, adrenal glands, adrenal cortex.</li>
+                </ul>
+            </div> <!-- /.info-col__box -->
+        </div>
+        <div class="col-lg-7">
+            <div class="info-col__content pl-xl-3 pl-lg-2">
+                <p>The Root Chakra is our ground, energetically speaking. It’s our foundation, our ability to stand on our two feet. It governs our ability to survive and to feel safe in the physical world. Because the root chakra is developed in early infancy, it is formed first. It contains all the information in those early months where the challenge is to be here, as a physical being.
+                </p>
+                <p>
+                    As a physical being, we experience hunger, discomfort and pain. The questions we answer are: ” Is this a safe place?” “Am I welcome here?” Everything in our surroundings pertaining to these questions gets stored in the root chakra.
+                </p>
+                <p>
+                    As we grow older, this sense of security manifests in the way we take care of ourselves. It plays out in our relationships with food, with shelter, with money – the things we need to survive.
+                </p>
+            </div> <!-- /.info-col__content -->
+        </div>
+    </div>
+</section> <!-- /.info-col -->
+
+<section class="banner-primary banner-primary--quote banner-primary--alt banner-primary--guide py-sm-3 py-2">
+    <div class="container">
+        <div class="banner-primary__inner bg-center text-white">
+            <div class="box-quote mx-auto max-width-850 text-center mt-1 mt-lg-3 px-lg-3 mb-sm-2 mb-1">
+                <p>“I feel like my sacral is shut off completely”, “I knew my sacral chakra has been blocked for a while and being able to put words and awareness on it and bringing intention to it has been priceless”</p>
+            </div>
+
+            <div class="text-wrap max-width-700 mx-auto text-sm-center">
+                <p>– Some words from participants in the first round of Chakra Journey in 2017. If you’re feeling unbalanced, you’re not alone and I promise you can absolutely come back to balance. </p>
+            </div>
+        </div>
+    </div>
+</section> <!-- banner-primary -->
+
+<section class="what-next container max-width-550 pb-xl-5 pb-lg-5 pb-sm-2 py-1">
+    <header class="what-next__heading mb-xl-5 mb-lg-4 mb-sm-3 mb-2">
+        <h2 class="heading-secondary mb-2 text-center">
+            <span>What</span>
+            to do next?
+        </h2>
+        <div class="btn-wrap text-center">
+            <a href="#" class="btn btn-primary">Check out this webinar</a>
+        </div>
+    </header> <!-- /.what-next__heading -->
+    <div class="what-next__video mb-sm-3 mb-2">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/9xwazD5SyVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div> <!-- /.what-next__video -->
+    <div class="what-next__info has-border pb-1 mb-2">
+        <h4>You’ll learn:</h4>
+        <ul>
+            <li>What the chakras are</li>
+            <li>What it looks like when they’re balanced<br> in which ways they can be out of balance and</li>
+            <li>Which questions to ask yourself to bring yourself back to balance!</li>
+        </ul>
+        <p>– You’ll have to input your e-mail, but that won’t necessarily subscribe you to the newsletter (unless you want it to).</p>
+    </div> <!-- /.what-next__info -->
+    <div class="what-next__info max-width-400 mx-auto mb-sm-3 mb-2">
+        <h4>Read These Related Articles</h4>
+        <ul>
+            <li>How Reiki Balances Your Chakras</li>
+            <li>Chakra Affirmations for an Unstoppably Vibrant You</li>
+            <li>How to Prepare for a Reiki Session</li>
+        </ul>
+        <div class="btn-wrap text-center">
+            <a href="#" class="btn btn-primary">Unlock this Exclusive Content</a>
+        </div>
+    </div> <!-- /.what-next__info -->
+    <div class="what-next__author text-center">
+        <h4>Book a Session with the Author</h4>
+        <p>Book A Session with Inge. Something like 50 words tying the blog post to the practitioner’s expertise and how they can help the client in the context of a session. Ends with a call to action. Filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text.</p>
+        <picture class="author-avatar">
+            <source srcset="<?php echo site_url(); ?>/media/author-avatar01.webp" type="image/webp">
+            <source srcset="<?php echo site_url(); ?>/media/author-avatar01.jpg" type="image/jpg">
+            <img width="80" height="80" src="<?php echo site_url(); ?>/media/author-avatar01.jpg" loading="lazy" alt="Gray Image">
+        </picture>
+        <h4>About the Author</h4>
+        <p>Something like 50 words tying the blog post to the practitioner’s expertise and how people can go further with them. Ends with a call to action. Filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text, filler text.</p>
+    </div>
+</section> <!-- /.what-next -->
 
 <?php
 get_footer();
