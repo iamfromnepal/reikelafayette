0a0ce399a7ae4b1d1a0525cdb4241d47ef17df29
Rajan Theengh <theenghrajan@gmail.com>
2022-07-06 01:52:12 +0545

    Practitionner page Desktop updated

--- www/wp-content/themes/reiki-lafayette/template-pages/tpl-practitioner.php
+++ www/wp-content/themes/reiki-lafayette/template-pages/tpl-practitioner.php
@@ -4,24 +4,122 @@
 get_header();
 ?>
 
-<section class="hero container-alt">
-    <div class="has-bg-img overlay overlay-80 overlay-80--top overlay-80--bottom">
-        <picture class="bg-img">
-            <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.webp" type="image/webp">
-            <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.jpg" type="image/jpg">
-            <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg-mobile.webp" type="image/webp">
-            <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg-mobile.jpg" type="image/jpg">
-            <img src="<?php echo site_url(); ?>/media/practitioner-hero-bg.jpg" alt="Home Hero Background" width="1440" height="865" loading="lazy">
-        </picture>
-        <div class="content-block d-flex flex-column mh-sm mh-lg mh-xl">
-            <h1 class="heading-primary text-center text-white">
-                Become
-                <span>Who you are</span>
-                with PRACTITIONER NAME
-            </h1>
-        </div>
+<section class="hero hero-prac container-alt has-bg-img overlay overlay-80 overlay-80--top overlay-80--bottom pt-5">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg-mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg-mobile.jpg" type="image/jpg">
+        <img src="<?php echo site_url(); ?>/media/practitioner-hero-bg.jpg" alt="Home Hero Background" width="1440" height="865" loading="lazy">
+    </picture>
+    <div class="container content-block d-flex flex-column">
+        <h1 class="heading-primary text-center text-white">
+            Become
+            <span>Who you are</span>
+            with PRACTITIONER NAME
+        </h1>
     </div>
 </section> <!-- /.hero -->
+
+<section class="prac-info pb-4">
+    <div class="container">
+        <div class="content-block mx-auto font-avenir font-300 px-1">
+            <div class="avatar-circle-wrapper bg-none offset-top-sm offset-top-lg mb-1">
+                <picture class="avatar-circle avatar-circle--sm rounded-circle circular-img">
+                    <source srcset="<?php echo site_url(); ?>/media/reiki-montreal-avatar.webp" type="image/webp">
+                    <source srcset="<?php echo site_url(); ?>/media/reiki-montreal-avatar.jpg" type="image/jpg">
+                    <img src="<?php echo site_url(); ?>/media/reiki-montreal-avatar.jpg" alt="Reiki Montreal Avatar" width="160" height="160" loading="lazy">
+                </picture>
+            </div>
+            <!-- /.avatar-circle-wrapper -->
+            <div class="text-center font-avenir font-300 font-size-lg">
+                <div class="title-h2-25 border-bottom">
+                    <h2>Inge Broer</h2>
+                    <p>The Alchemical Creatrix</p>
+                </div>
+                <div class="title-h2-25 border-bottom">
+                    <h2>Tools & Modalities:</h2>
+                    <p>Reiki, Psychotherapy, Cranio-Sacral, Tapping (and many more).</p>
+                </div>
+                <div class="title-h2-25 fborder-bottom">
+                    <h2>Expertise</h2>
+                    <p>XYZ specializes in helping XYZ overcome XYZ so that they can XYZ. </p>
+                </div>
+            </div>
+        </div> <!-- /.content-block -->
+
+        <div class="bleeding-block text-center has-bg-img pt-5 pb-1 px-2">
+            <picture class="bg-img">
+                <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.webp" type="image/webp">
+                <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.jpg" type="image/jpg">
+                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile-mobile.webp" type="image/webp">
+                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile-mobile.jpg" type="image/jpg">
+                <img src="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.jpg" alt="Prac Info Background" width="414" height="373" loading="lazy">
+            </picture>
+            <div class="inner">
+                <div class="btn-holder">
+                    <a href="#" class="btn btn-primary">Book A<br> Discover Session</a>
+                    <span>(Free, on the phone)</span>
+                </div>
+                <div class="btn-holder">
+                    <a href="#" class="btn btn-primary">Book A<br> Session</a>
+                    <span>(Paid, in person or online)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> <!-- /prac-info -->
+
+<section class="prac-schedule container pb-3">
+    <h2 class="heading-secondary text-center mb-7">
+        <span>Their</span>
+        Schedule
+    </h2>
+    <div class="row align-items-center justify-content-center two-columns">
+        <div class="col-lg-4">
+            <h3 class="heading-secondary title-bg">
+                <span>About</span>
+                First Last Name
+                <small>| In their words</small>
+            </h3>
+        </div>
+        <div class="col-lg-4 text-center">
+            <h3>Their Journey</h3>
+            <p>Aenean lacinia facilisis. Bibendum fusce. Ut turpis facilisi maecenas. Natoque mi, primis praesent. Facilisi feugiat torquent cras consequat dignissim habitant. Quam pharetra eu lobortis.<br>
+                Eu semper proin mollis litora dui. Feugiat morbi nulla elementum per justo lectus est purus magna. Dis nostra sodales Sollicitudin consectetuer et quam quam sollicitudin elementum luctus adipiscing quam sociis placerat sollicitudin placerat. Dignissim auctor morbi.<br>
+                Habitasse sapien lacinia. Tellus turpis cras hendrerit varius mus magnis est pharetra dapibus velit volutpat egestas parturient fermentum ad molestie nam suspendisse sit lacus adipiscing fusce venenatis. Massa ligula nostra. Ultrices nostra tristique lobortis. Vulputate amet. Erat mi ullamcorper fringilla.</p>
+        </div>
+    </div>
+</section> <!-- /.prac-schedule -->
+
+<section class="prac-bleeding container-alt has-bg-img pt-5 pb-3">
+    <picture class="bg-img">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.webp" type="image/webp">
+        <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile-mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile-mobile.jpg" type="image/jpg">
+        <img src="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.jpg" alt="Prac Bleeding Background" width="1440" height="865" loading="lazy">
+    </picture>
+    <div class="container content-block mx-auto text-center text-white">
+        <p>Eget nisl odio porta. Penatibus. Tortor interdum est nostra non porta ullamcorper curabitur. Suspendisse ultrices risus <br>
+            Fermentum torquent faucibus facilisis leo nec mattis tincidunt fusce ante viverra torquent interdum a. Facilisi congue<br>
+            Suscipit suspendisse sem.<br>
+            Nisi nisi fames integer semper urna metus conubia bibendum hac, ut, consequat congue egestas, adipiscing.</p>
+    </div>
+</section> <!-- /.parc-bleeding -->
+
+<section class="prac-bottom container pt-7 pb-4">
+    <div class="content-block text-center block-width-625 mx-auto">
+        <p>Pretium, diam rhoncus penatibus dolor sagittis libero faucibus senectus eget dapibus adipiscing dictum ligula libero natoque. Felis quis ipsum sollicitudin habitant phasellus consequat, egestas. Potenti lacinia felis. Pretium proin porta.
+            Amet porta dignissim ipsum. Habitant facilisi etiam. Amet pulvinar lacinia nonummy aliquet diam at fusce congue pretium pellentesque penatibus rhoncus nullam Dui enim netus inceptos tempus, condimentum sagittis.
+            Iaculis pulvinar, libero potenti justo quisque hymenaeos montes nostra Arcu quam morbi urna quam et. Venenatis. Est curabitur magnis sollicitudin venenatis luctus enim consequat molestie odio commodo quis nibh sem pellentesque pellentesque.
+        </p>
+        <h2 class="heading-secondary text-center pt-3">
+            <span>Their</span>
+            Schedule
+        </h2>
+    </div>
+</section> <!-- /.prac-bottom -->
 
 <?php
 get_footer();
