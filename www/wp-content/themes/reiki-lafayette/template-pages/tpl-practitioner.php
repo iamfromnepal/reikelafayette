e7ea770c2e16270ddf086aeb31a6905357619a1e
Rajan Theengh <theenghrajan@gmail.com>
2022-07-06 10:20:50 +0545

    Practitionner page responsive udpates

--- www/wp-content/themes/reiki-lafayette/template-pages/tpl-practitioner.php
+++ www/wp-content/themes/reiki-lafayette/template-pages/tpl-practitioner.php
@@ -4,7 +4,7 @@
 get_header();
 ?>
 
-<section class="hero hero-prac container-alt has-bg-img overlay overlay-80 overlay-80--top overlay-80--bottom pt-5">
+<section class="hero hero-prac container-alt has-bg-img overlay overlay-80 overlay-80--top overlay-80--bottom pt-4 pt-sm-5">
     <picture class="bg-img">
         <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.webp" type="image/webp">
         <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/practitioner-hero-bg.jpg" type="image/jpg">
@@ -21,7 +21,7 @@ get_header();
     </div>
 </section> <!-- /.hero -->
 
-<section class="prac-info pb-4">
+<section class="prac-info pb-2 pb-sm-4">
     <div class="container">
         <div class="content-block mx-auto font-avenir font-300 px-1">
             <div class="avatar-circle-wrapper bg-none offset-top-sm offset-top-lg mb-1">
@@ -52,8 +52,8 @@ get_header();
             <picture class="bg-img">
                 <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.webp" type="image/webp">
                 <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.jpg" type="image/jpg">
-                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile-mobile.webp" type="image/webp">
-                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile-mobile.jpg" type="image/jpg">
+                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.webp" type="image/webp">
+                <source srcset="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.jpg" type="image/jpg">
                 <img src="<?php echo site_url(); ?>/media/prac-info-bg01-mobile.jpg" alt="Prac Info Background" width="414" height="373" loading="lazy">
             </picture>
             <div class="inner">
@@ -71,12 +71,12 @@ get_header();
 </section> <!-- /prac-info -->
 
 <section class="prac-schedule container pb-3">
-    <h2 class="heading-secondary text-center mb-7">
+    <h2 class="heading-secondary text-center mb-3 mb-sm-7">
         <span>Their</span>
         Schedule
     </h2>
     <div class="row align-items-center justify-content-center two-columns">
-        <div class="col-lg-4">
+        <div class="col-lg-4 mb-1">
             <h3 class="heading-secondary title-bg">
                 <span>About</span>
                 First Last Name
@@ -92,12 +92,12 @@ get_header();
     </div>
 </section> <!-- /.prac-schedule -->
 
-<section class="prac-bleeding container-alt has-bg-img pt-5 pb-3">
+<section class="prac-bleeding container-alt has-bg-img pt-5 pb-2 pb-sm-3">
     <picture class="bg-img">
         <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.webp" type="image/webp">
         <source media="(min-width: 576px)" srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.jpg" type="image/jpg">
-        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile-mobile.webp" type="image/webp">
-        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile-mobile.jpg" type="image/jpg">
+        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.webp" type="image/webp">
+        <source srcset="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.jpg" type="image/jpg">
         <img src="<?php echo site_url(); ?>/media/prac-bleeding-bg01-mobile.jpg" alt="Prac Bleeding Background" width="1440" height="865" loading="lazy">
     </picture>
     <div class="container content-block mx-auto text-center text-white">
@@ -108,13 +108,13 @@ get_header();
     </div>
 </section> <!-- /.parc-bleeding -->
 
-<section class="prac-bottom container pt-7 pb-4">
+<section class="prac-bottom container pt-9 pt-sm-7 pb-4">
     <div class="content-block text-center block-width-625 mx-auto">
         <p>Pretium, diam rhoncus penatibus dolor sagittis libero faucibus senectus eget dapibus adipiscing dictum ligula libero natoque. Felis quis ipsum sollicitudin habitant phasellus consequat, egestas. Potenti lacinia felis. Pretium proin porta.
             Amet porta dignissim ipsum. Habitant facilisi etiam. Amet pulvinar lacinia nonummy aliquet diam at fusce congue pretium pellentesque penatibus rhoncus nullam Dui enim netus inceptos tempus, condimentum sagittis.
             Iaculis pulvinar, libero potenti justo quisque hymenaeos montes nostra Arcu quam morbi urna quam et. Venenatis. Est curabitur magnis sollicitudin venenatis luctus enim consequat molestie odio commodo quis nibh sem pellentesque pellentesque.
         </p>
-        <h2 class="heading-secondary text-center pt-3">
+        <h2 class="heading-secondary text-center pt-2 pt-sm-3">
             <span>Their</span>
             Schedule
         </h2>
